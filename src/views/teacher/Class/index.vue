0df4db5e49fd88a2d61b2d9f789f08bf520827f5
leijingfeng <1272172505@qq.com>
2026-04-10 11:14:43 +0800

    fix()班级tabmock数据

--- src/views/teacher/Class/index.vue
+++ src/views/teacher/Class/index.vue
@@ -5,11 +5,11 @@
         <div class="app_top_left_text">我的班级</div>
         <img src="@/assets/images/home/add.png" class="app_top_left_icon" alt="">
       </div>
-      <div class="app_container_box_left_search_box">
-        <div class="app_container_box_left_search_box_top">
+      <div class="app_container_box_left_search_box" v-if="isOpenSearch == false">
+        <div class="app_container_box_left_search_box_top" @click="openSearch()">
           <img src="@/assets/images/class/s_icon.png" class="app_container_box_left_search_box_top_icon" alt="">
-          <div class="app_container_box_left_search_box_top_input">
-            <input type="text" placeholder="搜索">
+          <div class="app_container_box_left_search_box_top_text">
+            搜索
           </div>
         </div>
         <div class="app_container_box_left_search_box_center">
@@ -39,30 +39,74 @@
           </div>
         </div>
       </div>
-      <div class="app_container_box_left_list">
-        <div class="app_container_box_left_list_detail app_container_box_left_list_detail_active">
-          <img src="@/assets/images/class/chooseYes.png" class="app_container_box_left_list_detail_chooseIcon" alt="">
-          <div class="app_container_box_left_list_detail_title">中国近现代史</div>
-          <div class="app_container_box_left_list_detail_count">657人</div>
+      <div  class="app_container_box_left_search_box" v-if="isOpenSearch==true">
+        <div class="app_container_box_left_search_box_topBox">
+           <div class="app_container_box_left_search_box_topBox_search">
+            <img src="@/assets/images/class/s_icon.png" class="app_container_box_left_search_box_top_icon" alt="">
+            <div class="app_container_box_left_search_box_top_input">
+              <input ref="searchInput" v-model="leftSearchText" type="text" :placeholder="searchType === 'class' ? '搜索班级' : '搜索学生'">
+            </div>
+          </div>
+          <div class="app_container_box_left_search_box_topBox_cancel" @click="isOpenSearch=false">取消</div>
         </div>
-         <div class="app_container_box_left_list_detail">
-          <img src="@/assets/images/class/chooseYes.png" class="app_container_box_left_list_detail_chooseIcon" alt="">
-          <div class="app_container_box_left_list_detail_title">中国近现代史</div>
-          <div class="app_container_box_left_list_detail_count">657人</div>
+        <div class="app_container_box_left_search_box_choose">
+          <div
+            class="app_container_box_left_search_box_choose_detail"
+            :class="{ 'app_container_box_left_search_box_choose_detail_active': searchType === 'class' }"
+            @click="searchType = 'class'"
+          >找班级</div>
+          <div
+            class="app_container_box_left_search_box_choose_detail"
+            :class="{ 'app_container_box_left_search_box_choose_detail_active': searchType === 'student' }"
+            @click="searchType = 'student'"
+          >找学生</div>
         </div>
-        <div class="app_container_box_left_list_detail">
-          <img src="@/assets/images/class/chooseYes.png" class="app_container_box_left_list_detail_chooseIcon" alt="">
-          <div class="app_container_box_left_list_detail_title">中国近现代史</div>
-          <div class="app_container_box_left_list_detail_count">657人</div>
+      </div>
+      <div class="app_container_box_left_list" v-if="isOpenSearch==false">
+        <div
+          class="app_container_box_left_list_detail"
+          :class="{ 'app_container_box_left_list_detail_active': selectedClassIndex === index }"
+          v-for="(item, index) in classList"
+          :key="index"
+          @click="selectClass(index)"
+        >
+          <img v-if="selectedClassIndex === index" src="@/assets/images/class/chooseYes.png" class="app_container_box_left_list_detail_chooseIcon" alt="">
+          <div class="app_container_box_left_list_detail_title">{{ item.name }}</div>
+          <div class="app_container_box_left_list_detail_count">{{ item.count }}人</div>
         </div>
+        <EmptyState v-if="classList.length === 0" description="暂无班级数据" />
+      </div>
+
+      <!-- 查询时的左边列表 -->
+      <div class="app_container_box_left_list" v-if="isOpenSearch==true">
+        <div class="search_tips" v-if="!leftSearchText &&searchType === 'class'">输入关键词查找班级</div>
+        <div class="search_tips" v-if="!leftSearchText &&searchType === 'student'">输入学生姓名/用户名/手机号查找学生所在班级</div>
+        <template v-if="leftSearchText">
+        <div
+          class="app_container_box_left_list_detail2"
+        
+          v-for="(item, index) in searchList"
+          :key="index"
+       
+        >
+          <div class="app_container_box_left_list_detail_title"><span class="app_container_box_left_list_detail_title_textCompare">铂金</span>班</div>
+          <div class="app_container_box_left_list_detail_count" v-if="searchType === 'student'"><span class="app_container_box_left_list_detail_title_textCompare">图图</span>老师</div>
+        </div>
+        </template>
+        <EmptyState v-if="leftSearchText && searchList.length === 0" description="无搜素结果" />
       </div>
     </div>
     <div class="app_container_box_right">
+      <!-- 无班级时空状态 -->
+      <div v-if="classList.length === 0" class="app_container_box_right_empty">
+        <EmptyState description="暂无班级，请先创建班级" />
+      </div>
+      <template v-if="classList.length > 0">
       <div class="app_container_box_right_top">
         <div class="app_container_box_right_top_top">
           <div class="app_container_box_right_top_top_left">
-            <div class="app_container_box_right_top_top_name">立升备课-管理学</div>
-            <div class="app_container_box_right_top_top_tag">剩余30天</div>
+            <div class="app_container_box_right_top_top_name">{{ currentClass.name }}</div>
+            <div class="app_container_box_right_top_top_tag">剩余{{ currentClass.remainDays }}天</div>
           </div>
           <div class="app_container_box_right_top_top_right">
             <img src="@/assets/images/class/rl.png" class="app_container_box_right_top_top_right_rl" alt="">
@@ -70,23 +114,23 @@
           </div>
         </div>
         <div class="app_container_box_right_top_mess">
-          <div>98人</div>
-          <div>2025-06-09  ~  2026-03-30 </div>
-          <div>后台创建</div>
+          <div>{{ currentClass.count }}人</div>
+          <div>{{ currentClass.startDate }}  ~  {{ currentClass.endDate }}</div>
+          <div>{{ currentClass.source }}</div>
         </div>
         <div class="app_container_box_right_top_choose">
-          <div class="app_container_box_right_top_choose_detail">
-            <div class="app_container_box_right_top_choose_detail_text app_container_box_right_top_choose_detail_text_active">学生</div>
-            <img src="@/assets/images/class/hx.png" class="app_container_box_right_top_choose_detail_hx" alt="">
+          <div class="app_container_box_right_top_choose_detail" @click="rightTab = 'student'">
+            <div class="app_container_box_right_top_choose_detail_text" :class="{ 'app_container_box_right_top_choose_detail_text_active': rightTab === 'student' }">学生</div>
+            <img v-if="rightTab === 'student'" src="@/assets/images/class/hx.png" class="app_container_box_right_top_choose_detail_hx" alt="">
           </div>
-          <div class="app_container_box_right_top_choose_detail">
-            <div class="app_container_box_right_top_choose_detail_text">课程</div>
-           
+          <div class="app_container_box_right_top_choose_detail" @click="rightTab = 'course'">
+            <div class="app_container_box_right_top_choose_detail_text" :class="{ 'app_container_box_right_top_choose_detail_text_active': rightTab === 'course' }">课程</div>
+            <img v-if="rightTab === 'course'" src="@/assets/images/class/hx.png" class="app_container_box_right_top_choose_detail_hx" alt="">
           </div>
         </div>
       </div>
       <!-- 学生tab列表 -->
-      <div class="app_container_box_right_last">
+      <div class="app_container_box_right_last" v-if="rightTab === 'student'">
         <div class="app_container_box_right_last_top">
           <div class="app_container_box_right_last_top_search">
             <img src="@/assets/images/class/s_icon.png" class="app_container_box_left_search_box_top_icon" alt="">
@@ -94,23 +138,23 @@
               <input type="text" placeholder="姓名/用户名/手机号">
             </div>
           </div>
-          <div class="app_container_box_right_last_top_num">共1个学生</div>
+          <div class="app_container_box_right_last_top_num">共{{ studentList.length }}个学生</div>
         </div>
         <div class="app_container_box_right_last_list">
-          <div class="app_container_box_right_last_list_detail" v-for="(item,index) in 10" :key="index">
+          <div class="app_container_box_right_last_list_detail" v-for="(item, index) in studentList" :key="index">
             <div class="app_container_box_right_last_list_detail_top">
               <img src="@/assets/images/class/options.png" class="app_container_box_right_last_list_detail_top_options" alt="">
               <div class="app_container_box_right_last_list_detail_top_mess">
                 <img src="@/assets/images/class/such.png" class="app_container_box_right_last_list_detail_top_mess_icon" alt="">
-                <div class="app_container_box_right_last_list_detail_top_mess_mess">立升备课-管理学牛牛牛牛牛牛</div>
+                <div class="app_container_box_right_last_list_detail_top_mess_mess">{{ item.name }}</div>
               </div>
             </div>
-            <div class="app_container_box_right_last_list_detail_last">进班日期：2025-03-26</div>
+            <div class="app_container_box_right_last_list_detail_last">进班日期：{{ item.joinDate }}</div>
           </div>
         </div>
       </div>
       <!-- 课程tab列表 -->
-      <!-- <div class="app_container_box_right_last">
+      <div class="app_container_box_right_last" v-if="rightTab === 'course'">
         <div class="app_container_box_right_last_top">
           <div class="app_container_box_right_last_top_search">
             <img src="@/assets/images/class/s_icon.png" class="app_container_box_left_search_box_top_icon" alt="">
@@ -118,22 +162,23 @@
               <input type="text" placeholder="搜索课程">
             </div>
           </div>
-          <div class="app_container_box_right_last_top_num">共1个课程</div>
+          <div class="app_container_box_right_last_top_num">共{{ courseList.length }}个课程</div>
         </div>
         <div class="app_container_box_right_last_list">
-          <div class="app_container_box_right_last_list_detailCourse" v-for="(item,index) in 10" :key="index">
-              <img src="@/assets/images/class/such.png" class="app_container_box_right_last_list_detailCourse_fm" alt="">
-              <div class="app_container_box_right_last_list_detailCourse_name">立升备课-管理学</div>
-              <div class="app_container_box_right_last_list_detailCourse_task">8个学习任务</div>
-              <div class="app_container_box_right_last_list_detailCourse_jd">
-                <div class="app_container_box_right_last_list_detailCourse_jd_box">
-                  <div class="app_container_box_right_last_list_detailCourse_jd_box_now" style="width:20%"></div>
-                </div>
-                <div class="app_container_box_right_last_list_detailCourse_jd_num">0%</div>
+          <div class="app_container_box_right_last_list_detailCourse" v-for="(item, index) in courseList" :key="index">
+            <img src="@/assets/images/class/such.png" class="app_container_box_right_last_list_detailCourse_fm" alt="">
+            <div class="app_container_box_right_last_list_detailCourse_name">{{ item.name }}</div>
+            <div class="app_container_box_right_last_list_detailCourse_task">{{ item.taskCount }}个学习任务</div>
+            <div class="app_container_box_right_last_list_detailCourse_jd">
+              <div class="app_container_box_right_last_list_detailCourse_jd_box">
+                <div class="app_container_box_right_last_list_detailCourse_jd_box_now" :style="{ width: item.progress + '%' }"></div>
               </div>
+              <div class="app_container_box_right_last_list_detailCourse_jd_num">{{ item.progress }}%</div>
+            </div>
           </div>
         </div>
-      </div> -->
+      </div>
+      </template>
     </div>
   </div>
 </template>
@@ -143,7 +188,112 @@ export default {
   name: 'Class' ,
   data(){
     return {
-      liveStatus:''
+      liveStatus:'',
+      isOpenSearch: false,
+      searchType: 'class',
+      rightTab: 'student',
+      selectedClassIndex: 0,
+      classList: [
+        {
+          name: '中国近现代史',
+          count: 657,
+          remainDays: 30,
+          startDate: '2025-06-09',
+          endDate: '2026-03-30',
+          source: '后台创建',
+          students: [
+            { name: '张伟', joinDate: '2025-03-26' },
+            { name: '李娜娜', joinDate: '2025-04-01' },
+            { name: '王小明', joinDate: '2025-04-10' },
+            { name: '刘晓燕', joinDate: '2025-05-15' },
+            { name: '陈思远', joinDate: '2025-06-02' },
+            { name: '赵雨涵', joinDate: '2025-06-18' },
+          ],
+          courses: [
+            { name: '立升备课-中国近现代史', taskCount: 12, progress: 55 },
+            { name: '立升备课-马克思主义基本原理', taskCount: 10, progress: 80 },
+            { name: '立升备课-大学语文', taskCount: 7, progress: 100 },
+          ]
+        },
+        {
+          name: '管理学精讲班',
+          count: 312,
+          remainDays: 120,
+          startDate: '2025-09-01',
+          endDate: '2026-06-30',
+          source: '教师创建',
+          students: [
+            { name: '孙浩然', joinDate: '2025-07-07' },
+            { name: '周婷婷', joinDate: '2025-08-20' },
+            { name: '吴思琪', joinDate: '2025-09-01' },
+            { name: '郑凯文', joinDate: '2025-09-05' },
+          ],
+          courses: [
+            { name: '立升备课-管理学', taskCount: 8, progress: 20 },
+            { name: '立升备课-思想道德与法治', taskCount: 9, progress: 40 },
+          ]
+        },
+        {
+          name: '马原理强化班',
+          count: 98,
+          remainDays: 5,
+          startDate: '2024-12-01',
+          endDate: '2025-06-01',
+          source: '后台创建',
+          students: [
+            { name: '王芳', joinDate: '2024-12-01' },
+            { name: '李强', joinDate: '2024-12-10' },
+          ],
+          courses: [
+            { name: '立升备课-马克思主义基本原理', taskCount: 10, progress: 80 },
+            { name: '立升备课-毛泽东思想概论', taskCount: 6, progress: 0 },
+          ]
+        },
+        {
+          name: '思想道德提升班',
+          count: 455,
+          remainDays: 200,
+          startDate: '2025-10-15',
+          endDate: '2026-09-15',
+          source: '教师创建',
+          students: [
+            { name: '刘浩宇', joinDate: '2025-10-15' },
+            { name: '陈晓雪', joinDate: '2025-10-20' },
+            { name: '杨帆', joinDate: '2025-11-01' },
+            { name: '黄晨曦', joinDate: '2025-11-08' },
+            { name: '林子涵', joinDate: '2025-11-20' },
+          ],
+          courses: [
+            { name: '立升备课-思想道德与法治', taskCount: 9, progress: 40 },
+            { name: '立升备课-大学语文', taskCount: 7, progress: 100 },
+          ]
+        },
+      ],
+      searchList:[],
+      leftSearchText:''
+    }
+  },
+  computed: {
+    currentClass() {
+      return this.classList[this.selectedClassIndex]
+    },
+    studentList() {
+      return this.currentClass ? this.currentClass.students : []
+    },
+    courseList() {
+      return this.currentClass ? this.currentClass.courses : []
+    }
+  },
+  methods:{
+    openSearch(){
+      this.isOpenSearch = true
+      this.$nextTick(() => {
+        this.$refs.searchInput && this.$refs.searchInput.focus()
+      })
+    },
+    selectClass(index) {
+      this.selectedClassIndex = index
+      this.rightTab = 'student'
     }
   }
 }
@@ -201,10 +351,16 @@ border: 1px solid #999999;
 gap: 8px;
 padding: 0 14px;
 box-sizing: border-box;
+cursor: pointer;
 }
 .app_container_box_left_search_box_top_icon{
   width: 16px;
   height: 16px;
+}
+.app_container_box_left_search_box_top_text{
+  font-weight: 400;
+font-size: 14px;
+color: #D9D9D9;
 }
 .app_container_box_left_search_box_top_input{
   flex: 1;
@@ -259,6 +415,21 @@ box-sizing: border-box;
 position: relative;
 cursor: pointer;
 }
+
+.app_container_box_left_list_detail2{
+  width: 300px;
+
+background: #FFFFFF;
+border-radius: 0px 0px 0px 0px;
+padding: 20px 30px 20px 16px;
+box-sizing: border-box;
+position: relative;
+cursor: pointer;
+}
+
+.app_container_box_left_list_detail2:hover{
+  background: #F0F3F6!important;
+}
 .app_container_box_left_list_detail_active{
   background: #CAD9FF!important;
 }
@@ -266,7 +437,7 @@ cursor: pointer;
   background: #F0F3F6!important;
 }
 .app_container_box_left_list_detail_title{
-  font-weight: bold;
+  font-weight: 400;
   width: 100%;
   text-overflow: ellipsis;
   white-space: nowrap;
@@ -293,6 +464,14 @@ margin-top: 9px;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+}
+.app_container_box_right_empty{
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #FFFFFF;
+  border-left: 1px solid #F3F4F8;
 }
 .app_container_box_right_top{
   width: 100%;
@@ -553,5 +732,74 @@ color: #666666;
   font-size: 12px;
   
 
+}
+.app_container_box_left_search_box_topBox{
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  gap: 14px;
+}
+
+.app_container_box_left_search_box_topBox_search{
+  flex: 1;
+  width: 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  height: 36px;
+background: #FFFFFF;
+border-radius: 28px 28px 28px 28px;
+border: 1px solid #0049FF;
+gap: 8px;
+padding: 0 14px;
+box-sizing: border-box;
+cursor: pointer;
+}
+.app_container_box_left_search_box_topBox_cancel{
+  font-weight: 400;
+font-size: 14px;
+color: #0049FF;
+cursor: pointer;
+}
+.app_container_box_left_search_box_choose{
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 32px;
+  margin-top: 16px;
+}
+.app_container_box_left_search_box_choose_detail{
+ width: 73px;
+ height: 36px;
+  font-weight: 400;
+font-size: 14px;
+display: flex;
+justify-content: center;
+align-items: center;
+color: #666666;
+background: transparent;
+border-radius: 18px 18px 18px 18px;
+cursor: pointer;
+}
+.app_container_box_left_search_box_choose_detail_active{
+  font-weight: 400;
+font-size: 14px;
+color: #333333;
+background: #D9D9D9;
+border-radius: 18px 18px 18px 18px;
+}
+.search_tips{
+  padding: 67px 40px 0 40px;
+  box-sizing: border-box;
+  text-align: center;
+  font-weight: 400;
+font-size: 14px;
+color: #666666;
+}
+
+.app_container_box_left_list_detail_title_textCompare{
+  color: #0049FF;
 }
 </style>
