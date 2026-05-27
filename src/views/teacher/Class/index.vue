f70606c00014298ebc958063f5d9dca42e7be349
chenguotian <1058520616@qq.com>
2026-05-28 01:10:54 +0800

    style: 直播iframe增加背景深色

--- src/views/teacher/Class/index.vue
+++ src/views/teacher/Class/index.vue
@@ -733,7 +733,7 @@
     <!-- 直播间 -->
     <div v-if="showLiveIframe" class="live-iframe-overlay">
       
-      <iframe :src="liveUrl" style="width: 100%; height: 100vh;" frameborder="0" allowfullscreen allow="camera;microphone;autoplay;display-capture;" allowusermedia></iframe>
+      <iframe :src="liveUrl" style="width: 100%; height: 100vh; background: #1E1E1E;" frameborder="0" allowfullscreen allow="camera;microphone;autoplay;display-capture;" allowusermedia></iframe>
     </div>
 
 
